4f6407a4a3cd2723baf4129760446bbb10086554
jastring <tripp.stringfield@gmail.com>
2019-10-23 14:06:13 -0400

    setup server.js connection.js schema.sql seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Hunka Hunka Burning Love");
+INSERT INTO burgers (burger_name) VALUES ("RoadHouse BBQ");
+INSERT INTO burgers (burger_name) VALUES ("Green Monster");
