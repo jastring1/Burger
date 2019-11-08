3051b44706f3689d1494eaa798fa285181b2f967
jastring <tripp.stringfield@gmail.com>
2019-11-08 13:51:59 -0500

    added some styling

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -5,21 +5,18 @@ const burger = require("../models/burger");
 router.get("/", function(req, res){
     burger.selectAll(function(data) {
         var returnObj = { burgers: data }
-        console.log(returnObj);
         res.render("index", returnObj)
     })
 })
 
 router.post("/api/burger", function(req, res){
     burger.insertOne(req.body.burger_name, function(data) {
-        console.log(req.body);
         res.status(204).end()
     })
 })
 
 router.put("/api/burger",function(req, res){
     burger.updateOne(req.body.id, function(data) {
-        console.log(req.body);
         res.status(204).end()
     })
 })
